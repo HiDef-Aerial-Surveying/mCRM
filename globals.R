fac08337eb4e0d69b5943b6ece011f828f466168
grwhumphries <grant.humphries@hidefsurveying.co.uk>
2022-03-29 12:56:14 +0100

    global variables

---
+++ globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c("defaultSpeciesValues",
+                         "all_lines",
+                         "all_polygons",
+                         "Scotwind_Merged",
+                         "startUpValues"))
